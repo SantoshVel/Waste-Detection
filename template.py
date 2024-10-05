77f9d405599a79f2a8765fc46857ad615dbc42f3
SantoshVel <santoshvel555@gmail.com>
2024-10-05 10:56:08 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+logger = logging.getLogger()
+
+project_name = "WasteDetection"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "data/.gitkeep",
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/constant/training_pipeline/__init__.py",
+    f"{project_name}/constant/application.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifacts_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "templates/index.html",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "tests.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logger.info(f"Creating directory: {filedir} for file: {filename}")
+
+    # Corrected line here
+    if (not os.path.exists(filepath) or os.path.getsize(filepath) == 0):  # Fixed parentheses and condition
+        with open(filepath, 'w') as f:
+            pass
+        logger.info(f"Creating empty file: {filepath}")
+    else:
+        logger.info(f"{filename} already exists")
+
+logger.info("Project setup completed.")
+
+
