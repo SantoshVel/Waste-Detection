eec908285e64808cf9a357933aa4607ee2c166be
SantoshVel <santoshvel555@gmail.com>
2024-10-06 11:06:06 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='WasteDetection',
+    version='0.0.0',
+    author='Santosh Vel',
+    author_email='santoshvel555@gmail.com',
+    packages=find_packages(),  # Corrected to 'packages'
+    install_requires=[],       # You can specify any required packages here
+)
